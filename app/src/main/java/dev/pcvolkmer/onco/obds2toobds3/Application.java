e03b7584f6b89733a3beaa3f2b0d3f51453c5a82
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-12-05 17:16:25 +0100

    Add application to map ADT_GEKID into oBDS 3

---
+++ app/src/main/java/dev/pcvolkmer/onco/obds2toobds3/Application.java
@@ -0,0 +1,39 @@
+package dev.pcvolkmer.onco.obds2toobds3;
+
+import org.apache.commons.cli.*;
+import org.apache.commons.io.input.BOMInputStream;
+import org.apache.commons.io.IOUtils;
+
+import de.basisdatensatz.obds.v2.ADTGEKID;
+import dev.pcvolkmer.onko.obds2to3.ObdsMapper;
+
+import java.io.File;
+import java.nio.file.*;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        final var options = new Options();
+        options.addOption(
+                Option.builder("i").argName("input").hasArg().desc("Input file").converter(File::new).build());
+        options.addOption(
+                Option.builder("o").argName("output").hasArg().desc("Output file").converter(File::new).build());
+
+        final var parsedCliArgs = DefaultParser.builder().build().parse(options, args);
+
+        if (parsedCliArgs.hasOption("h")) {
+            new HelpFormatter().printHelp("obds2toobds3 --input <input file> --output <output file>", options);
+        } else {
+            var input = Paths.get(parsedCliArgs.getOptionValue("i"));
+            var output = Paths.get(parsedCliArgs.getOptionValue("o"));
+
+            var mapper = new ObdsMapper();
+            var bomInputStream = BOMInputStream.builder().setInputStream(Files.newInputStream(input)).get();
+
+            var inputObj = mapper.readValue(IOUtils.toString(bomInputStream), ADTGEKID.class);
+            var mappedString = mapper.writeMappedXmlString(inputObj);
+
+            Files.writeString(output, mappedString);
+        }
+
+    }
+}
