19c4adbbd9c9a2c31a54ae036256a5c4d472de1d
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-12-06 08:08:19 +0100

    Show exceptions if -v flag is set

--- app/src/main/java/dev/pcvolkmer/onco/obds2toobds3/Application.java
+++ app/src/main/java/dev/pcvolkmer/onco/obds2toobds3/Application.java
@@ -18,22 +18,33 @@ public class Application {
                 Option.builder("i").argName("input").hasArg().desc("Input file").converter(File::new).build());
         options.addOption(
                 Option.builder("o").argName("output").hasArg().desc("Output file").converter(File::new).build());
+        options.addOption(
+                Option.builder("v").desc("Show exceptions and stack traces").build());
 
         final var parsedCliArgs = DefaultParser.builder().build().parse(options, args);
 
-        if (parsedCliArgs.hasOption("h")) {
+        if (parsedCliArgs.hasOption("h") || !parsedCliArgs.hasOption("input") || !parsedCliArgs.hasOption("output")) {
             new HelpFormatter().printHelp("obds2toobds3 --input <input file> --output <output file>", options);
         } else {
-            var input = Paths.get(parsedCliArgs.getOptionValue("i"));
-            var output = Paths.get(parsedCliArgs.getOptionValue("o"));
+            try {
+                var input = Paths.get(parsedCliArgs.getOptionValue("i"));
+                var output = Paths.get(parsedCliArgs.getOptionValue("o"));
 
-            var mapper = new ObdsMapper();
-            var bomInputStream = BOMInputStream.builder().setInputStream(Files.newInputStream(input)).get();
+                var mapper = new ObdsMapper();
+                var bomInputStream = BOMInputStream.builder().setInputStream(Files.newInputStream(input)).get();
 
-            var inputObj = mapper.readValue(IOUtils.toString(bomInputStream, StandardCharsets.UTF_8), ADTGEKID.class);
-            var mappedString = mapper.writeMappedXmlString(inputObj);
+                var inputObj = mapper.readValue(IOUtils.toString(bomInputStream, StandardCharsets.UTF_8),
+                        ADTGEKID.class);
+                var mappedString = mapper.writeMappedXmlString(inputObj);
 
-            Files.writeString(output, mappedString);
+                Files.writeString(output, mappedString);
+            } catch (Exception e) {
+                System.err.println("Konvertierung fehlgeschlagen");
+                if (parsedCliArgs.hasOption("v")) {
+                    throw e;
+                }
+            }
+
         }
 
     }
