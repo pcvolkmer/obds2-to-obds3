4db467097d351e8b0e73779ed548ba1090904198
chgl <chgl@users.noreply.github.com>
2025-12-03 10:03:53 +0100

    spotless

--- lib/src/test/java/io/github/bzkf/obds2toobds3/MeldungMapperTest.java
+++ lib/src/test/java/io/github/bzkf/obds2toobds3/MeldungMapperTest.java
@@ -1,13 +1,6 @@
 package io.github.bzkf.obds2toobds3;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import de.basisdatensatz.obds.v2.ADTGEKID;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class MeldungMapperTest {
 
